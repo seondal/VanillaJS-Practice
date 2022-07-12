14c0d7a9bb82e22a095cc573554ba0d64e92196a
seondal <sseondal@gmail.com>
2022-07-12 16:30:51 +0900

    Events

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
+const link = document.querySelector("a");
+
 function onLoginBtnClick() {
   const value = loginInput.value;
   console.log(value);
 }
+
+function onLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+  alert("clicked");
+}
+
 loginButton.addEventListener("click", onLoginBtnClick);
+link.addEventListener("click", onLinkClick);

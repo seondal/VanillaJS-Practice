880226ace9bc69efe6578665cc9ebd42dc5b0571
seondal <sseondal@gmail.com>
2022-07-15 06:00:20 +0900

    Challenge5_Random

---
+++ Challenge5_Random/app.js
@@ -0,0 +1,29 @@
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  const randomColor1 = colors[Math.floor(Math.random() * colors.length)];
+  const randomColor2 = colors[Math.floor(Math.random() * colors.length)];
+
+  const randomColor = `linear-gradient(${randomColor1}, ${randomColor2})`;
+  document.body.style = `background: ${randomColor};`;
+});
